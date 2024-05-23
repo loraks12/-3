003edf146cd57c1d6a02f4b8da50127b8d678761
loraks <val4evtushenko@yandex.ru>
2024-05-24 00:25:51 +0300

    добавление класса лдя хранения данных

---
+++ лр3/populationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лр3
+{
+    public class populationData
+    {
+        public int Year { get; set; }
+        public int Population { get; set; }
+    }
+}
