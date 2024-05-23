f2be73311df6fc9ed6a7a9c93d933df959176ff8
loraks <val4evtushenko@yandex.ru>
2024-05-24 01:35:22 +0300

    добавлены классы для работы с данными

---
+++ лр3/DataManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace лр3
+{
+    public class DataManager
+    {
+        public List<PopulationData> PopulationRecords {  get; private set; }
+
+        public DataManager(string filePath) {
+            
+            LoadData(filePath);
+        }
+        private void LoadData(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            PopulationRecords = lines.Skip(1) // Пропускаем заголовок
+                                      .Select(line => line.Split(','))
+                                      .Select(parts => new PopulationData
+                                      {
+                                          Year = int.Parse(parts[0]),
+                                          Population = int.Parse(parts[1])
+                                      })
+                                      .ToList();
+        }
+        public (double maxIncrease, int maxIncreaseYear, double maxDecrease, int maxDecreaseYear) CalculateGrowth()
+        {
+            double maxIncrease = 0;
+            double maxDecrease = 0;
+            int maxIncreaseYear = 0;
+            int maxDecreaseYear = 0;
+
+            for (int i = 1; i < PopulationRecords.Count; i++)
+            {
+                double change = ((double)(PopulationRecords[i].Population - PopulationRecords[i - 1].Population) 
+                    / PopulationRecords[i - 1].Population) * 100;
+                if (change > maxIncrease)
+                {
+                    maxIncrease = change;
+                    maxIncreaseYear = PopulationRecords[i].Year;
+                }
+                if (change < maxDecrease)
+                {
+                    maxDecrease = change;
+                    maxDecreaseYear = PopulationRecords[i].Year;
+                }
+            }
+
+            return (maxIncrease, maxIncreaseYear, maxDecrease, maxDecreaseYear);
+        }
+    }
+}
---
+++ лр3/PopulationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лр3
+{
+    public class PopulationData
+    {
+        public int Year {  get; set; }
+        public int Population { get; set; }
+    }
+}
